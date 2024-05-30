70ccec3afc17f985c8b4993fd4db09e57e4e920c
Prerna <prernassp@gmail.com>
2024-05-30 23:34:02 +0530

    Added the code

---
+++ Check_subdomain.py
@@ -0,0 +1,36 @@
+import requests
+import time
+from tabulate import tabulate
+
+# List of subdomains to check
+subdomains = [
+    'https://www.netflix.com/in/',
+    'https://www.netflix.com/login',
+    'https://www.netflix.com/signup/registration?locale=en-IN',
+]
+
+def check_subdomain_status(subdomains):
+    status_list = []
+    for subdomain in subdomains:
+        try:
+            response = requests.get(subdomain, timeout=5)
+            status = 'Up' if response.status_code == 200 else 'Down'
+        except requests.RequestException:
+            status = 'Down'
+        status_list.append((subdomain, status))
+    return status_list
+
+def display_status_table(status_list):
+    #table = tabulate(status_list, headers=['Subdomain', 'Status'], tablefmt='grid')
+   # print(table)
+   print("Error")
+
+def main():
+    while True:
+        status_list = check_subdomain_status(subdomains)
+        display_status_table(status_list)
+        print(status_list)
+        time.sleep(60)  # Wait for 1 minute before checking again
+
+if __name__ == "__main__":
+    main()
